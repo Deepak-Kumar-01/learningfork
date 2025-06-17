ffd41849b8d67bbcca9656d53cb86c0d2389341f
Deepak Kumar <deepakkumar224336@gmail.com>
2025-06-17 15:02:57 +0530

    changes-1

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Test Conflict branch'),
+      home: const MyHomePage(title: 'Test Conflict branch-changes 1'),
     );
   }
 }
